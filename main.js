67d705e8fb7f5687e11cf7c784b62c83dbcfffca
Sye-5pace <owusilasyp00@gmail.com>
2024-06-25 16:58:57 +0100

    feat: create loading screen redirecting to dashboard

---
+++ main.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const appLogo = document.getElementById('welcome-content')
+    const loginCard = document.getElementById('user-login')
+  
+    //redirects to Dashboard
+    const redirectLoadingScreen = (delay, targetPage) => {
+      setTimeout(() => {
+        window.location.href = targetPage;
+      }, delay);
+    };
+  
+    redirectLoadingScreen(5000, './dashboard.html');
+  });
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: ['index.html','Energy_Monitor.html'],
   theme: {
+    fontFamily: {
+      'glitch':['Rubik Glitch','system-ui'],
+      'ubuntu': ['Ubuntu','sans-serif']
+    },
+    colors:{
+      'tuatara':'#1b1b1a',
+      'whitelilac':'#fbfafd',
+      'honeysuckle':'#ECFB7A',
+      'whisper':'#F5F6FA'
+    },
     extend: {},
   },
   plugins: [],
